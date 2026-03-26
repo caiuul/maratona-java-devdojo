58ae4eb5634de447a24089356361a2929f6145e3
Caiul <caiulpsilva@gmail.com>
2026-03-25 21:12:29 -0300

    Exercicios sobre classes internas

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/ZZAclassesInternas/test/InnerClassesTest01.java
@@ -0,0 +1,8 @@
+package academy.devdojo.maratonajava.javacore.ZZAclassesInternas.test;
+
+public class InnerClassesTest01 {
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Zgenerics/test/ClasseGenericaTest03.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratonajava.javacore.Zgenerics.test;
+
+import academy.devdojo.maratonajava.javacore.Zgenerics.dominio.Barco;
+import academy.devdojo.maratonajava.javacore.Zgenerics.dominio.Carro;
+import academy.devdojo.maratonajava.javacore.Zgenerics.service.BarcoRentavelService;
+import academy.devdojo.maratonajava.javacore.Zgenerics.service.RentalService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClasseGenericaTest03 {
+    public static void main(String[] args) {
+
+        List<Carro> carrosDisponiveis = new ArrayList<>(List.of(new Carro("BWM"), new Carro("Fusca")));
+        List<Barco> barcosDisponiveis = new ArrayList<>(List.of(new Barco("lancha"), new Barco("iate")));
+        RentalService<Carro> rentalService = new RentalService<>(carrosDisponiveis);
+
+    }
+}
